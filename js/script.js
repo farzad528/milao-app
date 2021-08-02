ff9910ed6f663416e47b0400c13a3355d0aad955
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-08-02 08:06:22 -0500

    ADD: data working

--- js/script.js
+++ js/script.js
@@ -14,54 +14,37 @@ function getJobs() {
     });
 }
 
-function filterJobs(jobs, searchText) {
-  if (searchText) {
-    let filteredJobs = jobs.filter((job) => {
-      if (
-        job.roleName.toLowerCase().includes(searchText) ||
-        job.type.toLowerCase.includes(searchText) ||
-        job.company.toLowerCase.includes(searchText) ||
-        job.requirements.content.toLowerCase.includes(searchText)
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-    return filteredJobs;
-  } else {
-    return jobs;
-  }
-}
-
 function showJobs(jobs) {
-  let jobsContainer = document.querySelector(".jobs-contianer");
+  console.log(jobs);
+  let jobsContainer = document.querySelector(".jobs-container");
   let jobsHTML = "";
 
   jobs.forEach((job) => {
     jobsHTML += `
-      <div class="job-tile">
-          <div class="top">
-            <img
-              src="${job.logo}"
-              alt="top"
-            />
-            <i class="fas fa-ellipsis-h"></i>
-          </div>
-          <div class="rolename">
-            <span>${job.roleName}</span>
-          </div>
-          <div class="description">
-            <span>${job.requirements.content}</span>
-          </div>
-          <div class="buttons">
-            <div class="button apply-now">Apply Now</div>
-            <div class="button">Message</div>
-          </div>
-        </div>`;
+    <div class="job-tile">
+    <div class="top">
+      <img
+        src="${job.logo}"
+        alt="top"
+      />
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+    <div class="rolename">
+      <span>${job.roleName}</span>
+    </div>
+    <div class="description">
+      <span
+        >${job.requirements.content}
+      </span>
+    </div>
+    <div class="buttons">
+      <div class="button apply-now">Apply Now</div>
+      <div class="button">Message</div>
+    </div>
+  </div>`;
   });
-  jobsContainer.innerHTML = jobsHTML;
-  console.log(jobsHTML);
+
+  jobsContainer.innerHTML = jobsHTML
 }
 
 getJobs().then((data) => {
